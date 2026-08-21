f4d434357e75cfbe61eb527084d11afa47371a80
vtu29262-oss <vtu29262@veltech.edu.in>
2026-08-21 11:26:46 +0530

    Add files via upload

---
+++ session-9 Top K Frequent Elements  .java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        int k = 2;
+
+        Map<Integer, Integer> frequency = new HashMap<>();
+
+        for (int num : nums) {
+            frequency.put(num, frequency.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer>[] buckets = new ArrayList[nums.length + 1];
+
+        for (int num : frequency.keySet()) {
+            int freq = frequency.get(num);
+
+            if (buckets[freq] == null) {
+                buckets[freq] = new ArrayList<>();
+            }
+
+            buckets[freq].add(num);
+        }
+
+        int[] result = new int[k];
+        int index = 0;
+
+        for (int freq = buckets.length - 1; freq >= 0; freq--) {
+            if (buckets[freq] != null) {
+                for (int num : buckets[freq]) {
+                    result[index++] = num;
+
+                    if (index == k) {
+                        break;
+                    }
+                }
+            }
+
+            if (index == k) {
+                break;
+            }
+        }
+
+        System.out.println(Arrays.toString(result));
+    }
+}
+
+
+Your Output
+[1, 2]
